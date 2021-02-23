042f8657180dca6d9b9259741a8e8265020843f0
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-23 18:42:22 +0530

    Added: Calculate length based on end points

--- lineProblem.java
+++ lineProblem.java
@@ -1,8 +1,34 @@
+import java.util.Scanner;
 public class lineProblem {
 
-	public static void main(String[] args) {
+	static double length1;
 
-		System.out.println("Welcome to Line Comparision Computation Problem");
+	public static double length(int x1, int y1, int x2, int y2) {
 
+		length1 = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
+
+		return length1;
+	}
+
+	public static void main (String[] args) {
+
+		System.out.println("Welcome to Line Comparison Computation Problem");
+
+			int x1, y1, x2, y2;
+
+			Scanner input = new Scanner(System.in);
+			System.out.println("Co-ordinates for Line1");
+			System.out.println("Enter x1: ");
+			x1 = input.nextInt();
+			System.out.println("Enter y1: ");
+			y1 = input.nextInt();
+			System.out.println("Enter x2: ");
+			x2 = input.nextInt();
+			System.out.println("Enter y2: ");
+			y2 = input.nextInt();
+
+			length(x1 ,y1, x2, y2);
+
+			System.out.println("Length of Line1: "+length1);
 	}
 }
