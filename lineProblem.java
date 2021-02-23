d4a9b2c824bf8a903d340218b38ee59504283921
nikhilkumbhare <nikhilk190697@gmail.com>
2021-02-23 19:09:04 +0530

    Added: Checking Equality based on End Points

--- lineProblem.java
+++ lineProblem.java
@@ -1,34 +1,55 @@
 import java.util.Scanner;
 public class lineProblem {
 
-	static double length1;
+	static Double length1, length2;
 
-	public static double length(int x1, int y1, int x2, int y2) {
+	public static void length(int x1, int y1, int x2, int y2, int m1, int n1, int m2, int n2) {
 
 		length1 = Math.sqrt(((x2-x1)*(x2-x1)) + ((y2-y1)*(y2-y1)));
-
-		return length1;
+		length2 = Math.sqrt(((m2-m1)*(m2-m1)) + ((n2-n1)*(n2-n1)));
 	}
+
+	static void equality() {
+		boolean result = length1.equals(length2);
+			if(result) {
+				System.out.println("Line1 and Line2 are equal in Length");
+			}else {
+				System.out.println("Line1 and Line2 are not equal in Length");
+			}
+	}
+
 
 	public static void main (String[] args) {
 
 		System.out.println("Welcome to Line Comparison Computation Problem");
 
-			int x1, y1, x2, y2;
 
 			Scanner input = new Scanner(System.in);
 			System.out.println("Co-ordinates for Line1");
 			System.out.println("Enter x1: ");
-			x1 = input.nextInt();
+			int x1 = input.nextInt();
 			System.out.println("Enter y1: ");
-			y1 = input.nextInt();
+			int y1 = input.nextInt();
 			System.out.println("Enter x2: ");
-			x2 = input.nextInt();
+			int x2 = input.nextInt();
 			System.out.println("Enter y2: ");
-			y2 = input.nextInt();
+			int y2 = input.nextInt();
 
-			length(x1 ,y1, x2, y2);
+			System.out.println("Co-ordinates for Line1");
+			System.out.println("Enter m1: ");
+			int m1 = input.nextInt();
+			System.out.println("Enter n1: ");
+			int n1 = input.nextInt();
+			System.out.println("Enter m2: ");
+			int m2 = input.nextInt();
+			System.out.println("Enter n2: ");
+			int n2 = input.nextInt();
+
+			length(x1 ,y1, x2, y2, m1, n1, m2, n2);
 
 			System.out.println("Length of Line1: "+length1);
+			System.out.println("Length of Line2: "+length2);
+
+			equality();
 	}
 }
